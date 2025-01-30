f13d67b44bf3620b0caf05f24b2f79e1a0d6a529
Patrick-FO <patrick.olausson@gmail.com>
2025-01-30 23:55:52 +0100

    After certificate setup, if this dont work im cooked bro

--- server/index.js
+++ server/index.js
@@ -5,19 +5,21 @@ import trackRoutes from './routes/trackRoutes.js';
 import fileUpload from 'express-fileupload';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
+
+app.set('trust proxy', 1);
 
 app.use(cors({
-  origin: ['https://patrick-fo.github.io', 'http://localhost:5173'], 
+  origin: ['https://patrick-fo.github.io', 'http://localhost:5173', 'http://production-env.eba-fbx3qqzr.eu-north-1.elasticbeanstalk.com'], 
   methods: ['GET', 'POST', 'DELETE'],
   credentials: true
 }));
 
+app.use(express.json());
 app.use(fileUpload({
   limits: { fileSize: 50 * 1024 * 1024 }, 
   useTempFiles: true
 }));
-app.use(express.json());
 app.use('/api/tracks', trackRoutes);
 
 app.get('/', (req, res) => {
