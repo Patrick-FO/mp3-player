19a92c11c41fe9e5375054570596eaf989a518d0
Patrick-FO <patrick.olausson@gmail.com>
2025-01-30 17:36:02 +0100

    Update API URL

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://production-env.eba-fbx3qqzr.eu-north-1.elasticbeanstalk.com';
+export const API_BASE_URL = 'https://production-env.eba-fbx3qqzr.eu-north-1.elasticbeanstalk.com';
