c50a0cbcba9bd71db18b664a8787574ab66f06f4
Patrick-FO <patrick.olausson@gmail.com>
2025-01-31 22:45:23 +0100

    Found railway URL, changed port from 5000 to 8080 in index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import trackRoutes from './routes/trackRoutes.js';
 import fileUpload from 'express-fileupload';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.set('trust proxy', 1);
 
